159c66c14c973489bd0804b634ea82fca2f760d7
lhf <liaohongfei@hotmail.com>
2025-02-04 18:33:58 +0800

    start

---
+++ compose/test.sh
@@ -0,0 +1,14 @@
+docker run -d -p 3000:8080 \
+--add-host=host.docker.internal:host-gateway \
+-v open-webui:/app/backend/data \
+--name open-webui \
+--restart always ghcr.io/open-webui/open-webui:main
+
+
+
+docker run -d -p 3000:8080 \
+ --gpus all \
+ --add-host=host.docker.internal:host-gateway \
+ -v open-webui:/app/backend/data \
+ --name open-webui \
+ --restart always ghcr.io/open-webui/open-webui:cuda
