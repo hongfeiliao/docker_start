86af786c30e65ef7d93901fd659e7fbfaaea0cca
lhf <liaohongfei@hotmail.com>
2025-02-04 18:51:12 +0800

    20250204

--- compose/tmp.sh
+++ compose/tmp.sh
@@ -12,3 +12,9 @@ docker run -d -p 3000:8080 \
  -v open-webui:/app/backend/data \
  --name open-webui \
  --restart always ghcr.io/open-webui/open-webui:cuda
+
+
+docker run -d -p 58000:8000 -p 9443:9443 \
+--name portainer --restart=always \
+-v /var/run/docker.sock:/var/run/docker.sock \
+-v portainer_data:/data portainer/portainer-ce:latest
